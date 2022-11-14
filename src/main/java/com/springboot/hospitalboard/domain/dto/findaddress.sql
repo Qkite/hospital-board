1e618d4bf50f5e668e0e7282cdfab8a06ca44859
Qkite <yeonjigu4@gmail.com>
2022-11-14 13:23:00 +0900

    주소에서 경기도 수원시 찾기

--- src/main/java/com/springboot/hospitalboard/domain/dto/findaddress.sql
+++ src/main/java/com/springboot/hospitalboard/domain/dto/findaddress.sql
@@ -1,1 +1,1 @@
-SELECT * FROM `likelion-db`.nation_wide_hospitals WHERE road_name_address LIKE "경기도 수원시%";
+SELECT * FROM `likelion-db`.nation_wide_hospitals WHERE road_name_address LIKE "경기도 수원시%" AND hospital_name LIKE "%피부과%";
